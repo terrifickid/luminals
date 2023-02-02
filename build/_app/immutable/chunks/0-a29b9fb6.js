6d7a5ffc3df7403f861808004ca594387f8252ea
msplint <tk@terrifickid.net>
2023-02-01 21:47:13 -0500

    update

---
+++ build/_app/immutable/chunks/0-a29b9fb6.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_layout-5e18666d.js";import{default as t}from"../components/layout.svelte-87593943.js";export{t as component,r as universal};
--- build/_app/immutable/chunks/2-dab8dd44.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-13d353df.js";export{t as component};
